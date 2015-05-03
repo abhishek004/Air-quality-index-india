10e5403f978d3a60817a7a63935354229c06fd04
abhishek004 <abhishekjain1995@gmail.com>
2015-05-04 01:28:41 +0530

    URLs alloted to markers

---
+++ AQI.php
@@ -0,0 +1,107 @@
+<?php
+    include_once('simple_html_dom.php'); 
+    error_reporting(E_ERROR | E_PARSE);
+    function getAQI($I_low, $I_high, $C_low, $C_high, $C){
+        return round((($I_high - $I_low)/($C_high - $C_low))*($C - $C_low) + $I_low);
+    }
+
+    //echo getAQI(0,50,0,0.064,0.00804);
+    //$html = file_get_html("http://cpcb.gov.in/CAAQM/frmCurrentDataNew.aspx?StationName=Talkatora&StateId=28&CityId=256");
+
+    $aqi = array(
+            "Ozone" => array(138, 178, 220, 262, 790, 1070, 1280),
+            "PM 10" => array(54, 154, 254, 354, 424, 504, 604),
+            "PM 2.5" => array(15.4, 40.4, 65.4, 150.4, 250.4, 350.4, 500.4),
+            "Carbon Monoxide" => array(5.43, 11.6, 15.3, 19.0, 37.5, 49.8, 62.5),
+            "Sulfur Dioxide" => array(95, 406, 632, 857, 1700, 2280, 2832),
+            "Nitrogen Dioxide" => array(0, 0, 0, 1320, 2510, 3320, 4130),
+            
+            "AQI" => array(50, 100, 150, 200, 250, 300, 350)
+        
+        );
+
+    function getIndex($str, $val){
+        global $aqi;
+        for($i =0; $i < 7; $i++){
+            if($aqi[$str][$i] >= $val){
+                $high = $aqi[$str][$i];
+                $I_high = $aqi["AQI"][$i];
+                //echo $high." ".$I_high."<br>";
+                if($i != 0){
+                    $low = $aqi[$str][$i-1]+1;
+                    $I_low = $aqi["AQI"][$i-1]+1;
+                }else{
+                    //echo $low." ".$I_low."<br>";
+                    $low = 0;
+                    $I_low = 0;
+                }
+                break;
+            }
+        }
+
+        return getAQI($I_low, $I_high, $low, $high, $val);
+    }
+
+    //function for removing special characters:
+    function clean($string) {
+        return preg_replace('/[^A-Za-z0-9\\xB5\-\/]/', '', $string); // Removes special chars.
+    }
+    
+    $max = array(
+        "gas" => "",
+        "aqi" => 0
+    ); //most prominent pollutant
+    function fill_table($url){
+        //$url = "http://www.cpcb.gov.in/CAAQM/frmCurrentDataNew.aspx?StationName=Dwarka&StateId=6&CityId=85";
+        $dom = new DOMDocument();
+        $dom->loadHTMLFile($url);
+
+        $xpath = new DOMXpath($dom);
+
+        $table = $xpath->query(".//*[@id='lblReportCurrentData']/table/tr");
+        global $max;
+        global $aqi;
+        //echo "Gas name, concentration value, unit, concentration(previous 24 hours)/prescribed standard, AQI Value<br>";
+        //echo '<table class="table table-bordered">';
+        for($index = 2; $index < $table->length; $index++){
+            $gas = $xpath->query(".//*[@id='lblReportCurrentData']/table/tr[$index]/td[1]/text()");
+            //echo $gas->item(0)->nodeValue." ";
+            $concentration = $xpath->query(".//*[@id='lblReportCurrentData']/table/tr[$index]/td[4]/span/text()");
+            //echo $concentration->item(0)->nodeValue." ";
+            $unit = $xpath->query(".//*[@id='lblReportCurrentData']/table/tr[$index]/td[5]/text() ");   
+            //echo clean($unit->item(0)->nodeValue)." ";
+            $prescribed = $xpath->query(".//*[@id='lblReportCurrentData']/table/tr[$index]/td[6]/span/text() ");
+            //echo $prescribed->item(0)->nodeValue." ";
+            $result = getIndex($gas->item(0)->nodeValue, $concentration->item(0)->nodeValue);
+
+            if(array_key_exists($gas->item(0)->nodeValue,$aqi)){
+                echo "<tr>";
+                echo "<td>" . $gas->item(0)->nodeValue . "</td>";
+                echo "<td>" . $result . "</td>";
+                echo "</tr>";
+                if($result > $max["aqi"]){
+                    $max["gas"] = $gas->item(0)->nodeValue;
+                    $max["aqi"] = $result;
+                }
+            }
+        }
+        //echo "</table>";
+    }
+
+    function display_result(){
+        global $max;
+        echo 'Most prominent pollutant is ==> '.$max["gas"].'<br>'."With AQI: ".$max["aqi"];
+        
+    }
+    //echo "<br>".getIndex("PM 2.5", 29.18);
+    
+    // foreach($html->find('#Td1 table') as $aa){
+    //  foreach($aa->find('tr') as $row){
+    //      foreach($row->find('td') as $col){
+
+    //          echo $col->plaintext."******";
+    //      }
+    //      echo "<br>";
+    //  }
+    // }
+?>
--- index.php
+++ index.php
@@ -113,116 +113,7 @@
             </div><!--/.nav-collapse -->
           </div>
         </nav>
-
-        <?php
-            include_once('simple_html_dom.php'); 
-            error_reporting(E_ERROR | E_PARSE);
-            function getAQI($I_low, $I_high, $C_low, $C_high, $C){
-                return round((($I_high - $I_low)/($C_high - $C_low))*($C - $C_low) + $I_low);
-            }
-
-            //echo getAQI(0,50,0,0.064,0.00804);
-            //$html = file_get_html("http://cpcb.gov.in/CAAQM/frmCurrentDataNew.aspx?StationName=Talkatora&StateId=28&CityId=256");
-
-            $aqi = array(
-                    "Ozone" => array(138, 178, 220, 262, 790, 1070, 1280),
-                    "PM 10" => array(54, 154, 254, 354, 424, 504, 604),
-                    "PM 2.5" => array(15.4, 40.4, 65.4, 150.4, 250.4, 350.4, 500.4),
-                    "Carbon Monoxide" => array(5.43, 11.6, 15.3, 19.0, 37.5, 49.8, 62.5),
-                    "Sulfur Dioxide" => array(95, 406, 632, 857, 1700, 2280, 2832),
-                    "Nitrogen Dioxide" => array(0, 0, 0, 1320, 2510, 3320, 4130),
-                    
-                    "AQI" => array(50, 100, 150, 200, 250, 300, 350)
-                
-                );
-
-            function getIndex($str, $val){
-                global $aqi;
-                for($i =0; $i < 7; $i++){
-                    if($aqi[$str][$i] >= $val){
-                        $high = $aqi[$str][$i];
-                        $I_high = $aqi["AQI"][$i];
-                        //echo $high." ".$I_high."<br>";
-                        if($i != 0){
-                            $low = $aqi[$str][$i-1]+1;
-                            $I_low = $aqi["AQI"][$i-1]+1;
-                        }else{
-                            //echo $low." ".$I_low."<br>";
-                            $low = 0;
-                            $I_low = 0;
-                        }
-                        break;
-                    }
-                }
-
-                return getAQI($I_low, $I_high, $low, $high, $val);
-            }
-
-            //function for removing special characters:
-            function clean($string) {
-                return preg_replace('/[^A-Za-z0-9\\xB5\-\/]/', '', $string); // Removes special chars.
-            }
-            
-            $max = array(
-                "gas" => "",
-                "aqi" => 0
-            ); //most prominent pollutant
-            function fill_table($url){
-                //$url = "http://www.cpcb.gov.in/CAAQM/frmCurrentDataNew.aspx?StationName=Dwarka&StateId=6&CityId=85";
-                $dom = new DOMDocument();
-                $dom->loadHTMLFile($url);
-
-                $xpath = new DOMXpath($dom);
-
-                $table = $xpath->query(".//*[@id='lblReportCurrentData']/table/tr");
-                global $max;
-                global $aqi;
-                //echo "Gas name, concentration value, unit, concentration(previous 24 hours)/prescribed standard, AQI Value<br>";
-                //echo '<table class="table table-bordered">';
-                for($index = 2; $index < $table->length; $index++){
-                    $gas = $xpath->query(".//*[@id='lblReportCurrentData']/table/tr[$index]/td[1]/text()");
-                    //echo $gas->item(0)->nodeValue." ";
-                    $concentration = $xpath->query(".//*[@id='lblReportCurrentData']/table/tr[$index]/td[4]/span/text()");
-                    //echo $concentration->item(0)->nodeValue." ";
-                    $unit = $xpath->query(".//*[@id='lblReportCurrentData']/table/tr[$index]/td[5]/text() ");   
-                    //echo clean($unit->item(0)->nodeValue)." ";
-                    $prescribed = $xpath->query(".//*[@id='lblReportCurrentData']/table/tr[$index]/td[6]/span/text() ");
-                    //echo $prescribed->item(0)->nodeValue." ";
-                    $result = getIndex($gas->item(0)->nodeValue, $concentration->item(0)->nodeValue);
-
-                    if(array_key_exists($gas->item(0)->nodeValue,$aqi)){
-                        echo "<tr>";
-                        echo "<td>" . $gas->item(0)->nodeValue . "</td>";
-                        echo "<td>" . $result . "</td>";
-                        echo "</tr>";
-                        if($result > $max["aqi"]){
-                            $max["gas"] = $gas->item(0)->nodeValue;
-                            $max["aqi"] = $result;
-                        }
-                    }
-                }
-                //echo "</table>";
-            }
-
-            function display_result(){
-                global $max;
-                echo 'Most prominent pollutant is ==> '.$max["gas"].'<br>'."With AQI: ".$max["aqi"];
-                
-            }
-            //echo "<br>".getIndex("PM 2.5", 29.18);
-            
-            // foreach($html->find('#Td1 table') as $aa){
-            //  foreach($aa->find('tr') as $row){
-            //      foreach($row->find('td') as $col){
-
-            //          echo $col->plaintext."******";
-            //      }
-            //      echo "<br>";
-            //  }
-            // }
-        ?>
-
-
+        <?php include_once('AQI.php');?>
         <div class="container">
           <div class="starter-template">
             <div id="map" class="margin" style="width: 700px; height: 600px"></div>
@@ -252,7 +143,7 @@
             
         </div> -->
         
-            
+        
         
         <script>
 /*          
@@ -291,6 +182,30 @@
 
                 var markerIndex = 0, markersCoords = {};
                 var x=1;
+                var markTest = {
+                    'markers':[
+                        {latLng: [28.719804828832736,77.25068104282566], name: 'Dilshad Garden'},
+                        {latLng: [28.783376811716238,77.00896146131728], name:'D.C.E'},
+                        {latLng: [28.585246821204954,76.90802361409402], name:'Dwarka'},
+                        {latLng: [27.161830820078162,77.83037160828435], name: 'Agra(Sanjay Place)'},
+                        {latLng: [27.111786805400317,79.79768552106408], name:'Lucknow(Talkatora)'},
+                        {latLng: [26.303126672921213,79.75353238460825], name:'Kanpur(Nehru Nagar)'},
+                        {latLng: [25.371813455349116,82.89064557518128], name:'Varanasi(Ardhali Bazar)'},
+                        {latLng: [28.294864650453857,77.34307227196392], name:'Faridabad'},
+                        {latLng: [22.51450643032689,71.76962942369802], name:'Ahmedabad'}        
+                    ]
+                }
+
+                var urls=["http://www.cpcb.gov.in/CAAQM/frmCurrentDataNew.aspx?StationName=Ihbas&StateId=6&CityId=85",
+                            "http://www.cpcb.gov.in/CAAQM/frmCurrentDataNew.aspx?StationName=D.C.E.&StateId=6&CityId=85",
+                            "http: //www.cpcb.gov.in/CAAQM/frmCurrentDataNew.aspx?StationName=Dwarka&StateId=6&CityId=85",
+                            "http: //www.cpcb.gov.in/CAAQM/frmCurrentDataNew.aspx?StationName=Sanjay%20Palace&StateId=28&CityId=253",
+                            "http://cpcb.gov.in/CAAQM/frmCurrentDataNew.aspx?StationName=Talkatora&StateId=28&CityId=256",
+                            "http://cpcb.gov.in/CAAQM/frmCurrentDataNew.aspx?StationName=Nehru%20Nagar&StateId=28&CityId=278",
+                            "http://cpcb.gov.in/CAAQM/frmCurrentDataNew.aspx?StationName=Ardhali%20Bazar&StateId=28&CityId=270"
+                            ];
+
+             
                 var mar = {
                     'IN-DL': [
                         {latLng: [28.719804828832736,77.25068104282566], name: 'Dilshad Garden'},
@@ -354,7 +269,7 @@
                         setTimeout(function(){
                           x=0;  
                         },500);
-                        map.addMarkers(mar[c]);
+                        // map.addMarkers(markTest['markers']);
                     },
 
                     onViewportChange: function(e,s){
@@ -363,19 +278,26 @@
                         console.log(x);
                         map.removeAllMarkers(); 
                         x=1;
-                      }                    
+                      }
+                      else if(x===1 && s>2){
+                        map.addMarkers(markTest['markers']);
+                        x=0;
+                      }
+                      
                     },
 
                     onMarkerClick: function(e, c){
                     // here replace "#al" with your id for div!!
-                    $("#al").html(c+' is clicked: display AQI message');
+                    console.log(urls[c]);
+                    
+                    
                 }
 
                 });
 
                 map.series.regions[0].setValues(generateColors());
 
-               /* map.container.click(function(e){
+               /*map.container.click(function(e){
                     var latLng = map.pointToLatLng(e.offsetX, e.offsetY);
                     var targetCls = $(e.target).attr('class');
                     if (latLng && (!targetCls || (targetCls && $(e.target).attr('class').indexOf('jvectormap-marker') === -1))) {
